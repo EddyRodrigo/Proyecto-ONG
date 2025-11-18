000012aa6e2e79cb49319e1e9d06ba3be094d556
EddyRodrigo <eddy14rodrigo@gmail.com>
2025-11-18 14:56:52 -0400

    enlace de sql y migrate

--- ProyectoONG/settings.py
+++ ProyectoONG/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ong',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,18 @@ WSGI_APPLICATION = 'ProyectoONG.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ong',
+        'USER': 'root',
+        'PASSWORD': 'eddy14ck',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
+
 
 
 # Password validation
